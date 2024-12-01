cdddb3aa5550c78bd8207c300a460e0780fc3600
01821 <2949526614@qq.com>
2024-12-01 23:21:48 +0800

    更新至P6：添加阳光管理器

---
+++ Scenes/Cards/States/click.gd
@@ -0,0 +1,11 @@
+extends State
+
+func enter():
+	print("植物进入点击状态")
+
+
+func update(_delta: float):pass
+
+func physics_update(_delta: float):pass
+
+func exit():pass
---
+++ Scenes/Manager/sun_manager.gd
@@ -0,0 +1,20 @@
+class_name SunManager
+extends Node
+
+@export var start_sun := 10
+@export var sun_label:Label
+
+var UINode:UI
+var card_list:Array[CardTemplate]
+
+var sun:
+	set(value):
+		sun = value
+		sun_label.text = str(value)
+		for card in card_list:
+			card._is_sun_enough(value)
+
+func _ready() -> void:
+	UINode = get_tree().get_first_node_in_group("UI")
+	card_list = UINode.card_list
+	sun = start_sun
