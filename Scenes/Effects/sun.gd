144917d35d65293d7002f494d2fd323d42667627
01821 <2949526614@qq.com>
2024-12-07 14:45:22 +0800

    15、复用阳光生成组件，创建自然阳光，阳光自然衰减消失

--- Scenes/Effects/sun.gd
+++ Scenes/Effects/sun.gd
@@ -2,10 +2,16 @@ extends AnimatedSprite2D
 class_name Sun
 
 @export var collect_speed := 600
+@export var disappear_time := 25
+@onready var disappear_timer: Timer = $DisappearTimer
 
 var sun_num : int
 var is_click := false
 var UI_Node:UI
+
+func _ready() -> void:
+	disappear_timer.wait_time = disappear_time
+	disappear_timer.start()
 
 func _on_button_pressed() -> void:
 	if not is_click:
@@ -18,4 +24,12 @@ func _on_button_pressed() -> void:
 		_tween.tween_property(self,"position",end_pos,dua)
 		await _tween.finished
 		sun_manager.sun += sun_num
+		queue_free()
+
+func _on_disappear_timer_timeout() -> void:
+	if not is_click:
+		is_click = true
+		var _tween = create_tween()
+		_tween.tween_property(self,"modulate",Color(1,1,1,0),1)
+		await _tween.finished
 		queue_free()
